0a1b593d733cf9fb833921d52bc7ffae70df67e4
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2017-05-07 07:10:21 -0400

    Add files

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  DependencyInjection
+//
+//  Created by Jeff Kereakoglow on 5/7/17.
+//  Copyright © 2017 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ Source/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  DependencyInjection
+//
+//  Created by Jeff Kereakoglow on 5/7/17.
+//  Copyright © 2017 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+}
+
